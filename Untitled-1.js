1a818ff5d3bde7154d7bfe06294e3f5b80749c7b
hamna <hamna.gullazt@gmail.com>
2025-06-10 08:01:55 -0700

    Your message

--- Untitled-1.js
+++ Untitled-1.js
@@ -2,6 +2,7 @@ let emailhelp = "Your email must be in format: 'xyz@gmail.com'";
 let namehelp = "Your name must be in format: 'First-name second-name'";
 let phonehelp = "Your Phone number must be in format: '+923177594882'";
 
+
 const regexp=(email) =>{
   const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
   return regex.test(email);
@@ -34,33 +35,67 @@ const n =(id)=>{
   const btn=document.getElementById("submitbtn");
 btn.addEventListener("click",function(event){
     event.preventDefault();
-      val1 = "name"
-      val2 = "email"
-      val3 = "phone"
 
-const info = n(val1);
- document.getElementById("nameinfo").textContent+=`${info}  `;
+    
+btn.disabled = true
 
-const info1 = n(val2);
+const info = n("name");
+ document.getElementById("nameinfo").textContent=`Name is : ${info}  `;
+
+const info1 = n("email");
 if(regexp(info1) == true)
 {
- document.getElementById("emailinfo").textContent+=`${info1}  `;
+ document.getElementById("emailinfo").textContent=`Email is : ${info1}  `;
 }
 else{
     alert("Invalid Email. Please enter a valid email to continue.")
+        return;
 }
 
 
-const info2 = n(val3);
+const info2 = n("phone");
 if(regphone(info2) == true)
 {
- document.getElementById("phoneinfo").textContent+=`${info2}  `;
+ document.getElementById("phoneinfo").textContent=`Phone is : ${info2}  `;
 }
 else{
     alert("Invalid Phone Number. Please enter a valid Phone Number to continue.")
+        return;
 }
 }
 )
+
+document.getElementById("resetbtn").addEventListener("click", ()=>{
+document.getElementById("nameinfo").textContent = ""
+document.getElementById("emailinfo").textContent = ""
+document.getElementById("phoneinfo").textContent = ""
+}
+)
+
+const verify = ()=>{
+const validname = document.getElementById("name").value.trim();
+const validemail = document.getElementById("email").value.trim();
+const validphone = document.getElementById("phone").value.trim();
+
+btn.disabled = !((validname!=="" )
+  && (validemail != "" && regexp(validemail)) 
+  && validphone != "" && regphone(validphone))
+
+}
+
+const inp = ["name","email","phone"].map(id => document.getElementById(id))
+inp.forEach(inp => {
+inp.addEventListener("input",verify)
+verify();
+}
+)
+
+
+
+
+
+
+
 
 const formstyle=document.getElementById("form1");
 formstyle.style.display="flex";
